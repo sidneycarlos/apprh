9711396516419b9c8c3e53a692be94239f3be697
Lucas-knut <bwb.steichen.l@fondespierre.com>
2021-06-21 14:02:20 +0000

    pages added

--- index.php
+++ index.php
@@ -21,9 +21,7 @@
     include("./template/header.php");
     ?>
     <div class="w-full overflow-x-hidden border-t flex flex-col">
-        <main class="w-full flex-grow p-6">
-        
-        </main>
+        <!-- INCLUDE TA PAGE POUR TESTER -->
         <? include("./template/footer.php"); ?>
     </div>
 
---
+++ views/employees.php
@@ -0,0 +1,3 @@
+<main class="w-full flex-grow p-6">
+    <!-- INSÉRER SON CONTENU DANS LE MAIN -->
+</main>
