5712d57b46089484446c63ae049897bbf43b4824
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-06-21 20:29:08 +0000

    FIX template available employee card

--- views/employees.php
+++ views/employees.php
@@ -1,38 +1,115 @@
 <main class="w-full flex-grow p-6">
     <!-- INSÉRER SON CONTENU DANS LE MAIN -->
 
-    <!-- component card -->
-    <div class="container mx-auto max-w-sm flex flex-col space-x-4 justify-center items-center">
+    <!-- Big container -->
+    <div class="text-blueGray-700 antialiased">
+        
+        <!-- Smaller container -->
+        <div class="px-4 md:px-10 mx-auto w-full">
 
-        <div class="bg-white w-full flex items-center p-2 rounded-xl shadow border">
+            <!-- Cards -->
+            <div class="flex flex-wrap justify-around">
+                
+                <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                        <div class="flex-auto p-4">
+                            <div class="flex flex-wrap">
+                                <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                    <h5 class="font-semibold text-xl text-blueGray-700">
+                                        John
+                                    </h5>
+                                    <h5 class="font-semibold text-xl text-blueGray-700">
+                                        Doe
+                                    </h5>
+                                    <span class="text-blueGray-400 uppercase font-bold text-xs">
+                                        Manutention
+                                    </span>
+                                </div>
+                                <div class="relative w-auto pl-4 flex-initial">
+                                    <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-pink-500">
+                                        <i class="fas fa-user-astronaut fa-2x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="text-sm text-blueGray-400 mt-4">
+                                <button class="text-orange-500 mr-2">Disponible</button>
+                                <button type="button" class="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-2">
+                                    Affecter
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="flex items-center space-x-4">
-                <p class="w-16 h-16 rounded-full">
-                    <i class="fas fa-user-astronaut fa-4x"></i>
-                </p>
+                <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                        <div class="flex-auto p-4">
+                            <div class="flex flex-wrap">
+                                <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                    <h5 class="font-semibold text-xl text-blueGray-700">
+                                        Martine
+                                    </h5>
+                                    <h5 class="font-semibold text-xl text-blueGray-700">
+                                        Michu
+                                    </h5>
+                                    <span class="text-blueGray-400 uppercase font-bold text-xs">
+                                        Conception
+                                    </span>
+                                </div>
+                                <div class="relative w-auto pl-4 flex-initial">
+                                    <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-pink-500">
+                                        <i class="fas fa-user-astronaut fa-2x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="text-sm text-blueGray-400 mt-4">
+                                <button class="text-orange-500 mr-2">Disponible</button>
+                                <button type="button" class="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-2">
+                                    Affecter
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                        <div class="flex-auto p-4">
+                            <div class="flex flex-wrap">
+                                <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                    <h5 class="font-semibold text-xl text-blueGray-700">
+                                        Thomas
+                                    </h5>
+                                    <h5 class="font-semibold text-xl text-blueGray-700">
+                                        Dupont
+                                    </h5>
+                                    <span class="text-blueGray-400 uppercase font-bold text-xs">
+                                        Testeur
+                                    </span>
+                                </div>
+                                <div class="relative w-auto pl-4 flex-initial">
+                                    <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-pink-500">
+                                        <i class="fas fa-user-astronaut fa-2x"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="text-sm text-blueGray-400 mt-4">
+                                <button class="text-orange-500 mr-2">Disponible</button>
+                                <button type="button" class="w-full border text-base font-medium text-black bg-white hover:bg-gray-100 px-4 py-2">
+                                    Affecter
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
 
-            <div class="flex-grow p-3">
-
-            <div class="font-semibold text-gray-700">
-                Nom
-            </div>
-
-            <div class="font-semibold text-gray-700">
-                Prenom
-            </div>
-
-            <div class="font-semibold text-gray-700">
-                Poste
-            </div>
-            
-            <button type="button" class="w-1/3 px-4 py-2 text-base border rounded-lg text-white bg-indigo-500 hover:bg-indigo-700 ">
-                Affecter
-            </button>
-
-            </div>
         </div>
-
+    
+    
+    
     </div>
 
 </main>
