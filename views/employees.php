529472049b08f509f4e233d52e4cfe701fe2b63e
maialenwat <bwb.watrigant.m@fondespierre.com>
2021-06-21 15:11:59 +0000

    template card for available employee

--- views/employees.php
+++ views/employees.php
@@ -1,3 +1,38 @@
 <main class="w-full flex-grow p-6">
     <!-- INSÉRER SON CONTENU DANS LE MAIN -->
+
+    <!-- component card -->
+    <div class="container mx-auto max-w-sm flex flex-col space-x-4 justify-center items-center">
+
+        <div class="bg-white w-full flex items-center p-2 rounded-xl shadow border">
+
+            <div class="flex items-center space-x-4">
+                <p class="w-16 h-16 rounded-full">
+                    <i class="fas fa-user-astronaut fa-4x"></i>
+                </p>
+            </div>
+
+            <div class="flex-grow p-3">
+
+            <div class="font-semibold text-gray-700">
+                Nom
+            </div>
+
+            <div class="font-semibold text-gray-700">
+                Prenom
+            </div>
+
+            <div class="font-semibold text-gray-700">
+                Poste
+            </div>
+            
+            <button type="button" class="w-1/3 px-4 py-2 text-base border rounded-lg text-white bg-indigo-500 hover:bg-indigo-700 ">
+                Affecter
+            </button>
+
+            </div>
+        </div>
+
+    </div>
+
 </main>
