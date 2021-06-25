dbc455c9eea467f2df9c46552f3cc112f89e9535
Lucas-knut <bwb.steichen.l@fondespierre.com>
2021-06-25 09:24:04 +0000

    header fix

--- template/header.php
+++ template/header.php
@@ -1,7 +1,7 @@
 <!--HEADER-->
 <div class="w-full flex flex-col h-screen overflow-y-hidden">
         <!-- Desktop Header -->
-        <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
+        <header class="w-full items-center bg-white py-2 px-6 sm:flex">
             <div class="w-1/2"></div>
             <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
                 <button class="bg-gray-200 px-5 py-2 text-gray-600 text-sm shadow-sm font-medium tracking-wider border rounded-lg hover:shadow-lg hover:bg-gray-300">Déconnexion
