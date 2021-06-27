869fb11941cb93355b276539b40ad35035a9ac8c
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-06-27 06:14:05 +0000

    fix logo

--- template/navigation.php
+++ template/navigation.php
@@ -21,7 +21,7 @@
     <!--NAVIGATION-->
     <aside class="relative bg-sidebar h-screen w-64 hidden sm:block shadow-xl">
         <div class="p-6">
-            <a href="index.html" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">APP RH</a>
+            <a href="dashboard" class="text-white text-3xl font-semibold uppercase hover:text-gray-300">APP RH</a>
         </div>
         <nav class="text-white text-base font-semibold pt-3">
             <a href="dashboard" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
